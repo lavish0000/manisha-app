49c9f31d032a27d0cd5c49f53a85f59e4c2db913
lavish0000 <46993225+lavish0000@users.noreply.github.com>
2022-12-30 17:41:18 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+# Exit script if any command fails
+set -e
+
+# Function to handle errors
+error_handler() {
+  echo "Error: An error occurred at line $1"
+  exit 1
+}
+
+# Trap errors
+trap 'error_handler $LINENO' ERR
+
 # Check if Node.js is installed
 if ! command -v node &> /dev/null
 then
+    # Log message
+    echo "Installing Node.js..."
+
     # Install Node.js
     curl -sL https://deb.nodesource.com/setup_14.x | bash -
     apt-get install -y nodejs
@@ -11,9 +26,15 @@ fi
 # Check if Nginx is installed
 if ! command -v nginx &> /dev/null
 then
+    # Log message
+    echo "Installing Nginx..."
+
     # Install Nginx
     apt-get install -y nginx
 fi
+
+# Log message
+echo "Cloning Node.js server repository..."
 
 # Clone the Node.js server repository
 git clone https://github.com/peaqnetwork/peaq-did-raspberry-pi.git
@@ -21,11 +42,20 @@ git clone https://github.com/peaqnetwork/peaq-did-raspberry-pi.git
 # Navigate to the server directory
 cd peaq-did-raspberry-pi
 
+# Log message
+echo "Installing dependencies..."
+
 # Install dependencies
 npm i
 
+# Log message
+echo "Installing PM2..."
+
 # Install PM2
 npm install pm2@latest -g
+
+# Log message
+echo "Starting server using PM2..."
 
 # Start the server using PM2
 pm2 start index.js
@@ -41,3 +71,6 @@ startup_script=$(pm2 startup)
 
 # Execute the startup script
 eval "$startup_script"
+
+# Log success message
+echo "Script completed successfully!"
