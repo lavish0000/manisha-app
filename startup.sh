0653d52a38b928f6e8a924d3b1bc4216b5321ffa
lavish0000 <46993225+lavish0000@users.noreply.github.com>
2023-01-09 21:12:42 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 version=1.1
 
 # Check if the version is already stored in the system
-stored_version=$(cat /version 2>/dev/null)
+stored_version=$(cat version 2>/dev/null)
 
 run_script() {
   # Run the rest of the script
@@ -13,13 +13,13 @@ run_script() {
 
 if [ -z "$stored_version" ]; then
   # If the version is not stored, store it and run the script
-  sudo echo "$version" > /version
+  sudo echo "$version" > version
   run_script
 else
   # If the version is already stored, compare it to the current version
   if [ "$version" \> "$stored_version" ]; then
     # If the current version is greater than the stored version, update the stored version and run the script
-    sudo echo "$version" > /version
+    sudo echo "$version" > version
     run_script
   else
     # If the stored version is greater than or equal to the current version, skip running the script
