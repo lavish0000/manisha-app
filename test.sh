e1c18656e106acd8c096c54f2d263c5f8a514ab9
lavish0000 <46993225+lavish0000@users.noreply.github.com>
2023-01-11 22:02:48 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -89,7 +89,7 @@ echo "Starting server using PM2..."
 pm2 start index.js
 
 # Start the update script using PM2 which will be restarted every 5 seconds (update the time interval to 1 min)
-pm2 start bash --interpreter "sudo" --cron-restart="*/5 * * * * *"  --name "startup-script" -- -c "curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/lavish0000/manisha-app/main/startup.sh | bash"
+pm2 start bash --interpreter "sudo" --cron-restart="*/5 * * * * *"  --name "startup-script" -- -c "curl -H 'Cache-Control: no-cache, no-store' -s https://raw.githubusercontent.com/lavish0000/manisha-app/main/startup.sh | bash"
 
 # Save the PM2 process list	
 pm2 save
