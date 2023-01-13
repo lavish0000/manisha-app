f204fbed22c54c46a4eb73b63ecc241ff5b6f3a4
lavish0000 <46993225+lavish0000@users.noreply.github.com>
2023-01-13 21:32:31 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,34 +6,34 @@
 
 cd ~
 version=1.0
-stored_version=$(cat ./.update/version 2>/dev/null)
+stored_version=$(cat .update/version 2>/dev/null)
 
 run_script() {
   sudo mkdir test_auto_updates
 }
 
-if [ ! -d "./.update" ]; then
-  echo "Creating .update directory" | tee -a ./.update/logs.txt
-  sudo mkdir ./.update || { echo "Error: Failed to create .update directory" | tee -a ./.update/logs.txt; exit 1; }
+if [ ! -d ".update" ]; then
+  echo "Creating .update directory" | tee -a .update/logs.txt
+  sudo mkdir .update || { echo "Error: Failed to create .update directory" | tee -a .update/logs.txt; exit 1; }
 fi
 
 if [ -z "$stored_version" ]; then
-  echo "Creating version file" | tee -a ./.update/logs.txt
-  touch ./.update/version || { echo "Error: Failed to create version file" | tee -a ./.update/logs.txt; exit 1; }
-  echo "Setting permissions on version file" | tee -a ./.update/logs.txt
-  chmod 766 ./.update/version || { echo "Error: Failed to set permissions on version file" | tee -a ./.update/logs.txt; exit 1; }
-  echo "Writing version to version file" | tee -a ./.update/logs.txt
-  echo "$version" > ./.update/version || { echo "Error: Failed to write version to version file" | tee -a ./.update/logs.txt; exit 1; }
-  echo "Running script" | tee -a ./.update/logs.txt
-  run_script || { echo "Error: Failed to run script" | tee -a ./.update/logs.txt; exit 1; }
+  echo "Creating version file" | tee -a .update/logs.txt
+  touch .update/version || { echo "Error: Failed to create version file" | tee -a ./.update/logs.txt; exit 1; }
+  echo "Setting permissions on version file" | tee -a .update/logs.txt
+  chmod 766 .update/version || { echo "Error: Failed to set permissions on version file" | tee -a ./.update/logs.txt; exit 1; }
+  echo "Writing version to version file" | tee -a .update/logs.txt
+  echo "$version" > ./.update/version || { echo "Error: Failed to write version to version file" | tee -a .update/logs.txt; exit 1; }
+  echo "Running script" | tee -a .update/logs.txt
+  run_script || { echo "Error: Failed to run script" | tee -a .update/logs.txt; exit 1; }
 else
   if [ "$version" \> "$stored_version" ]; then
-    echo "Updating version in version file" | tee -a ./.update/logs.txt
-    echo "$version" > ./.update/version || { echo "Error: Failed to update version in version file" | tee -a ./.update/logs.txt; exit 1; }
-    echo "Running script" | tee -a ./.update/logs.txt
-    run_script || { echo "Error: Failed to run script" | tee -a ./.update/logs.txt; exit 1; }
+    echo "Updating version in version file" | tee -a .update/logs.txt
+    echo "$version" > .update/version || { echo "Error: Failed to update version in version file" | tee -a ./.update/logs.txt; exit 1; }
+    echo "Running script" | tee -a .update/logs.txt
+    run_script || { echo "Error: Failed to run script" | tee -a .update/logs.txt; exit 1; }
   else
-    echo "Skipping script because stored version ($stored_version) is greater than or equal to current version ($version)" | tee -a ./.update/logs.txt
+    echo "Skipping script because stored version ($stored_version) is greater than or equal to current version ($version)" | tee -a .update/logs.txt
   fi
 fi
 
